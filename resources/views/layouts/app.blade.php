647babcccab488967e64662efc97e5cdb2ee4baa
jacewoods <woodsjace@yahoo.com>
2019-10-27 21:16:57 -0700

    included bootstrap and new pages for cleaner look

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Acme</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+    @include('inc.navbar')
+
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 col-lg-8">
+        @yield('content')
+      </div>
+      <div class="col-md-4 col-lg-4">
+        @include('inc.sidebar')
+      </div>
+    </div>
+  </div>
+
+  </body>
+</html>
