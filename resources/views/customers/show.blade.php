691a8f844c190649ab1433178273e2e4cfe9ee05
jacewoods <woodsjace@yahoo.com>
2019-10-30 11:07:39 -0700

    updated individual customer record view data

--- resources/views/customers/show.blade.php
+++ resources/views/customers/show.blade.php
@@ -2,12 +2,50 @@
 
 @section('content')
   <h1>{{$customer->firstname}} {{$customer->lastname}}</h1>
-  <div>
-    {{$customer->email}}
-  </div>
+  <ul class ="list-group">
+    <li class="list-group-item">Email: {{$customer->email}}</li>
+
+    @if (is_null($customer->address1))
+    <li class="list-group-item">Address: <font color="#CCCCCC"><i>NULL</i></font></li>
+    @else
+    <li class="list-group-item">Address: {{$customer->address1}}</li>
+    @endif
+
+    @if (is_null($customer->address2))
+    <li class="list-group-item">Address 2: <font color="#CCCCCC"><i>NULL</i></font></li>
+    @else
+    <li class="list-group-item">Address 2: {{$customer->address2}}</li>
+    @endif
+
+    @if (is_null($customer->city))
+      <li class="list-group-item">City: <font color="#CCCCCC"><i>NULL</i></font></li>
+    @else
+      <li class="list-group-item">City: {{$customer->city}}</li>
+    @endif
+
+    @if (is_null($customer->state))
+    <li class="list-group-item">State: <font color="#CCCCCC"><i>NULL</i></font></li>
+    @else
+    <li class="list-group-item">State: {{$customer->state}}</li>
+    @endif
+
+    @if (is_null($customer->zip))
+    <li class="list-group-item">Zip: <font color="#CCCCCC"><i>NULL</i></font></li>
+    @else
+    <li class="list-group-item">Zip: {{$customer->zip}}</li>
+    @endif
+
+    @if (is_null($customer->country))
+    <li class="list-group-item">Country: <font color="#CCCCCC"><i>NULL</i></font></li>
+    @else
+    <li class="list-group-item">Country: {{$customer->country}}</li>
+    @endif
+
+  </ul>
 
 <hr>
 <small>Created at {{$customer->created_at}}</small>
+<small class="pull-right">Last updated at {{$customer->updated_at}}</small>
 <hr>
 <div class="btn-toolbar">
   <div class="btn-group mr-2">
